812e116a710d1d61bf6b63d32b58d8026f589c41
Pedrojonassm <Pedrojonassm@gmail.com>
2019-10-31 18:27:16 -0300

    atualizado n funcional

--- src/net/codejava/config/WebAppInitializer.java
+++ src/net/codejava/config/WebAppInitializer.java
@@ -9,11 +9,12 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 import org.springframework.web.servlet.DispatcherServlet;
  
 public class WebAppInitializer implements WebApplicationInitializer {
+    
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
         appContext.register(WebMvcConfig.class);
-          
+        
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet(
                 "SpringDispatcher", new DispatcherServlet(appContext));
         dispatcher.setLoadOnStartup(1);
--- src/net/codejava/customer/Customer.java
+++ src/net/codejava/customer/Customer.java
@@ -4,13 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
- 
+import javax.persistence.Table;
+
 @Entity
+@Table(name="customer") //nome da tabela
 public class Customer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id //diz qual é a chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment do banco de dados
     private Long id;
- 
     private String name;
     private String email;
     private String address;
@@ -23,7 +24,36 @@ public class Customer {
         this.email = email;
         this.address = address;
     }
- 
-    // getters and setters are not shown for brevity
- 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
